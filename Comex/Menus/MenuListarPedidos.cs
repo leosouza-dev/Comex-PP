04ac6fe731de7423498756276f4d7fb37d6674da
Leonardo Rodrigues de Souza <leonardo.souza@epharma.com.br>
2024-06-26 20:59:14 -0300

    Resolvendo semana 09 durante encontro ao vivo.

---
+++ Comex/Menus/MenuListarPedidos.cs
@@ -0,0 +1,22 @@
+﻿using Comex.Modelos;
+using Comex.Utils;
+
+namespace Comex.Menus
+{
+    public class MenuListarPedidos
+    {
+        public void ListarPedidos(List<Pedido> listaDePedidos)
+        {
+            Console.Clear();
+            Console.WriteLine("Exibindo todos os produtos registradoss na nossa aplicação");
+
+            var pedidosOrdenados = listaDePedidos.OrderBy(p => p.Cliente.Nome).ToList();
+
+            foreach (var Pedido in pedidosOrdenados)
+            {
+                Console.WriteLine($"Cliente: {Pedido.Cliente.Nome}, Total: {Pedido.Total:F2}");
+            }
+            ConsoleUtils.PausaELimpaATela();
+        }
+    }
+}
---
+++ Comex/Utils/ConsoleUtils.cs
@@ -0,0 +1,12 @@
+﻿namespace Comex.Utils
+{
+    public static class ConsoleUtils
+    {
+        public static void PausaELimpaATela()
+        {
+            Console.WriteLine("\nDigite uma tecla para voltar ao menu principal XD");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
